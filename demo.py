9014061eac8ed10aeaed97126b952583ce68e13c
Ashutoshh <ashutoshh@linux.com>
2020-12-22 10:11:14 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,9 +5,7 @@ app = Flask(__name__)
 def hello(): 
 	return "welcome to the flask tutorials"
 
-@app.route('/app')
-def app11():
-	return render_template("app.html")
+
 
 if __name__ == "__main__": 
 	app.run(host ='0.0.0.0', port = 5000, debug = True) 
